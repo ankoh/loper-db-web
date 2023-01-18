8ccd60602a1aad3b008e49abf5f6e7f8d2c0db25
André Kohn <andre.kohn@salesforce.com>
2023-01-18 16:15:43 +0100

    Rename build targets

--- packages/loper-db-web-extension/src/lib.rs
+++ packages/loper-db-web-extension/src/lib.rs
@@ -4,8 +4,37 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
+struct LoperService {}
+
+impl LoperService {
+    pub fn configure(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        Ok(cx.undefined())
+    }
+    pub fn open_connection(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        Ok(cx.undefined())
+    }
+    pub fn close_connection(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        Ok(cx.undefined())
+    }
+    pub fn create_session(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        Ok(cx.undefined())
+    }
+    pub fn close_session(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        Ok(cx.undefined())
+    }
+    pub fn execute_query(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        Ok(cx.undefined())
+    }
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("hello", hello)?;
+    cx.export_function("loper_service_configure", LoperService::configure)?;
+    cx.export_function("loper_service_open_connection", LoperService::open_connection)?;
+    cx.export_function("loper_service_close_connection", LoperService::close_connection)?;
+    cx.export_function("loper_service_create_session", LoperService::create_session)?;
+    cx.export_function("loper_service_close_session", LoperService::close_session)?;
+    cx.export_function("loper_service_execute_query", LoperService::execute_query)?;
     Ok(())
 }
