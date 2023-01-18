dcb04a217892d25b546c04ab56c020e497e62cd0
André Kohn <andre.kohn@salesforce.com>
2023-01-18 17:36:18 +0100

    Draft node api functions

--- packages/loper-db-web-app/src/backend/backend.ts
+++ packages/loper-db-web-app/src/backend/backend.ts
@@ -1,3 +1,5 @@
+/// Typedef for a local stream reader id
+export type LocalStreamReaderId = number;
 /// Typedef for a local connection id
 export type LocalConnectionId = number;
 /// Typedef for a local session id
@@ -5,41 +7,45 @@ export type LocalSessionId = number;
 /// Typedef for a global session id
 export type GlobalSessionId = string;
 
-/// An Arrow IPC stream that asynchronously provides IPC message buffers
-export interface ArrowIPCStreamReader {
-    /// Get the next  Arrow IPC chunk or return null if the end of the stream is reached.
-    next(): Promise<Uint8Array | null>;
-}
-
-/// A Loper service connection
-export interface LoperServiceConnectionDescriptor {
-    /// The local connection id used by the client
-    localConnectionId: LocalConnectionId;
-}
-
-/// A Loper session descriptor
-export interface LoperServiceSessionDescriptor {
-    /// The global session id sent to loper
-    globalSessionId: GlobalSessionId;
-    /// The local session id used by the client
-    localSessionId: LocalSessionId;
-}
-
 /// A Loper service backend
 export interface LoperServiceBackend {
     /// Configure the Loper service backen
     configure(): Promise<void>;
     /// Open a connection to a Loper service
-    openConnection(host: string): Promise<LoperServiceConnectionDescriptor>;
-    /// Close a connection to a Loper service
-    closeConnection(conn: LoperServiceConnectionDescriptor): Promise<LoperServiceSessionDescriptor>;
-    /// Create a session in a Loper connection
-    createSession(conn: LoperServiceConnectionDescriptor): Promise<LoperServiceSessionDescriptor>;
-    /// Close a session in a Loper connection
-    closeSession(session: LoperServiceSessionDescriptor): Promise<void>;
-    /// Execute a query in
-    executeQuery(session: LoperServiceSessionDescriptor, query: string): Promise<ArrowIPCStreamReader>;
+    createConnection(): Promise<LoperServiceConnection>;
 };
+
+/// A Loper service connection
+export interface LoperServiceConnection {
+    /// The local connection id used by the client
+    localConnectionId: LocalConnectionId;
+    /// Close a session in a Loper connection
+    close(): Promise<void>;
+    /// Open a connection to a Loper service
+    createSession(): Promise<LoperServiceSession>;
+}
+
+/// A Loper session descriptor
+export interface LoperServiceSession {
+    /// The global session id sent to loper
+    globalSessionId: GlobalSessionId;
+    /// The local session id used by the client
+    localSessionId: LocalSessionId;
+    /// Close a connection to a Loper service
+    close(): Promise<void>;
+    /// Execute a query in
+    executeQuery(query: string): Promise<QueryResultStreamReader>;
+}
+
+/// A reader for query results
+export interface QueryResultStreamReader {
+    /// The local connection id used by the client
+    localStreamReaderId: LocalStreamReaderId;
+    /// Close the result stream reader
+    close(): Promise<void>;
+    /// Get the next  Arrow IPC chunk or return null if the end of the stream is reached.
+    next(): Promise<Uint8Array | null>;
+}
 
 /// A backend for the Loper client application
 export interface Backend {
--- packages/loper-db-web-extension/src/lib.rs
+++ packages/loper-db-web-extension/src/lib.rs
@@ -1,40 +1,63 @@
 use neon::prelude::*;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
-}
+struct LoperServiceClient {}
 
-struct LoperService {}
-
-impl LoperService {
-    pub fn configure(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-        Ok(cx.undefined())
+impl LoperServiceClient {
+    pub fn configure(mut cx: FunctionContext) -> JsResult<JsObject> {
+        let service_info = cx.empty_object();
+        Ok(service_info)
     }
-    pub fn open_connection(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-        Ok(cx.undefined())
+    pub fn open_connection(mut cx: FunctionContext) -> JsResult<JsObject> {
+        let connection_descriptor = cx.empty_object();
+        Ok(connection_descriptor)
     }
     pub fn close_connection(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        let _connection_id = cx.argument::<JsNumber>(0)?.value(&mut cx);
         Ok(cx.undefined())
     }
-    pub fn create_session(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-        Ok(cx.undefined())
+    pub fn create_session(mut cx: FunctionContext) -> JsResult<JsObject> {
+        let _connection_id = cx.argument::<JsNumber>(0)?.value(&mut cx);
+        let session_descriptor = cx.empty_object();
+        Ok(session_descriptor)
     }
     pub fn close_session(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        let _connection_id = cx.argument::<JsNumber>(0)?.value(&mut cx);
+        let _session_id = cx.argument::<JsNumber>(1)?.value(&mut cx);
         Ok(cx.undefined())
     }
     pub fn execute_query(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        let _connection_id = cx.argument::<JsNumber>(0)?.value(&mut cx);
+        let _session_id = cx.argument::<JsNumber>(1)?.value(&mut cx);
+        let _session_id = cx.argument::<JsNumber>(2)?.value(&mut cx);
+        Ok(cx.undefined())
+    }
+    pub fn read_query_result_stream(mut cx: FunctionContext) -> JsResult<JsArrayBuffer> {
+        let _connection_id = cx.argument::<JsNumber>(0)?.value(&mut cx);
+        let _session_id = cx.argument::<JsNumber>(1)?.value(&mut cx);
+        let _session_id = cx.argument::<JsNumber>(2)?.value(&mut cx);
+        let _stream_id = cx.argument::<JsNumber>(3)?.value(&mut cx);
+
+        Ok(JsArrayBuffer::new(&mut cx, 1)?)
+    }
+    pub fn close_query_result_stream(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        let _connection_id = cx.argument::<JsNumber>(0)?.value(&mut cx);
+        let _session_id = cx.argument::<JsNumber>(1)?.value(&mut cx);
+        let _session_id = cx.argument::<JsNumber>(2)?.value(&mut cx);
+        let _stream_id = cx.argument::<JsNumber>(3)?.value(&mut cx);
         Ok(cx.undefined())
     }
 }
 
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
-    cx.export_function("loper_service_configure", LoperService::configure)?;
-    cx.export_function("loper_service_open_connection", LoperService::open_connection)?;
-    cx.export_function("loper_service_close_connection", LoperService::close_connection)?;
-    cx.export_function("loper_service_create_session", LoperService::create_session)?;
-    cx.export_function("loper_service_close_session", LoperService::close_session)?;
-    cx.export_function("loper_service_execute_query", LoperService::execute_query)?;
+    cx.export_function("loper_configure", LoperServiceClient::configure)?;
+    cx.export_function("loper_open_connection", LoperServiceClient::open_connection)?;
+    cx.export_function("loper_close_connection", LoperServiceClient::close_connection)?;
+    cx.export_function("loper_create_session", LoperServiceClient::create_session)?;
+    cx.export_function("loper_close_session", LoperServiceClient::close_session)?;
+    cx.export_function("loper_execute_query", LoperServiceClient::execute_query)?;
+    cx.export_function("loper_read_query_result_stream", LoperServiceClient::read_query_result_stream)?;
+    cx.export_function("loper_close_query_result_stream", LoperServiceClient::close_query_result_stream)?;
     Ok(())
 }
